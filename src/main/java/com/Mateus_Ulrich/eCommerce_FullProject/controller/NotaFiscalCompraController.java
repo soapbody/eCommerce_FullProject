31726b424bb50ba4896087eee0e784fbfd3f8d74
soapbody <mateus_teti98@hotmail.com>
2023-07-18 13:19:46 -0300

    estudando um pouco no intervalo do serviço...

--- src/main/java/com/Mateus_Ulrich/eCommerce_FullProject/controller/NotaFiscalCompraController.java
+++ src/main/java/com/Mateus_Ulrich/eCommerce_FullProject/controller/NotaFiscalCompraController.java
@@ -2,7 +2,9 @@ package com.Mateus_Ulrich.eCommerce_FullProject.controller;
 
 import com.Mateus_Ulrich.eCommerce_FullProject.exceptions.CustomException;
 import com.Mateus_Ulrich.eCommerce_FullProject.model.NotaFiscalCompra;
+import com.Mateus_Ulrich.eCommerce_FullProject.model.NotaFiscalVenda;
 import com.Mateus_Ulrich.eCommerce_FullProject.repository.NotaFiscalCompraRepository;
+import com.Mateus_Ulrich.eCommerce_FullProject.repository.NotaFiscalVendaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +19,8 @@ import java.util.List;
 public class NotaFiscalCompraController {
     @Autowired
     private NotaFiscalCompraRepository notaFiscalCompraRepository;
+    @Autowired
+    private NotaFiscalVendaRepository notaFiscalVendaRepository;
     @ResponseBody /*Poder dar um retorno da API*/
     @PostMapping(value = "**/salvarNotaFiscalCompra") /*Mapeando a url para receber JSON*/
     public ResponseEntity<NotaFiscalCompra> salvarNotaFiscalCompra(@RequestBody @Valid NotaFiscalCompra notaFiscalCompra) throws CustomException { /*Recebe o JSON e converte pra Objeto*/
@@ -73,6 +77,19 @@ public class NotaFiscalCompraController {
 
 
         return new ResponseEntity<NotaFiscalCompra>(notaFiscalCompra,HttpStatus.OK);
+    }
+    @ResponseBody
+    @GetMapping(value = "**/buscarNotaFiscalDaVenda/{id}")
+    public ResponseEntity<NotaFiscalVenda> buscarNotaFiscalDaVenda(@PathVariable("id") Long id) throws CustomException {
+
+        NotaFiscalVenda notaFiscalVenda = notaFiscalVendaRepository.buscaNotaPorVenda(id);
+        if (notaFiscalVenda == null) {
+            // Lida com o caso em que a nota fiscal de compra não foi encontrada
+            throw new CustomException("Nota fiscal de compra não encontrada");
+        }
+
+
+        return new ResponseEntity<NotaFiscalVenda>(notaFiscalVenda,HttpStatus.OK);
     }
 
     @ResponseBody
--- src/main/java/com/Mateus_Ulrich/eCommerce_FullProject/repository/NotaFiscalVendaRepository.java
+++ src/main/java/com/Mateus_Ulrich/eCommerce_FullProject/repository/NotaFiscalVendaRepository.java
@@ -7,8 +7,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface NotaFiscalVendaRepository extends JpaRepository<NotaFiscalVenda, Long> {
+    @Query(value = "select n from NotaFiscalVenda n where n.vendaCompraLojaVirtual.id = ?1")
+    List<NotaFiscalVenda> buscaNotaPorVendaList(Long id);
 
+    @Query(value = "select n from NotaFiscalVenda n where n.vendaCompraLojaVirtual.id = ?1")
+    NotaFiscalVenda buscaNotaPorVenda(Long id);
 }
